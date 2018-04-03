8f8156d3f809ef18fb8e637a9c24cf9733782447
Jeremiah <jeremiah3643@gmail.com>
2018-04-03 13:49:17 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const lastName= "Pritchard"
+if (lastName.length >= 7){
+    console.log("I can't pronounce this last name.")
+} 
+else (lastname.lenght < 7){
+    console.log("The last name is",lastName);
+}
+
+//lastNameResponder();
